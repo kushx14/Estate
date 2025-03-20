c0d5774c2df811ddff97cb4b3c129b29332780f2
kushx14 <157268371+kushx14@users.noreply.github.com>
2025-03-21 01:14:12 +0530

    tp middlewale

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
    const {username ,email,password}=req.body;
    const hashedpassword=bcryptjs.hashSync(password,10);
    const user = new User({username,email,password:hashedpassword});
@@ -8,7 +8,7 @@ export const signup=async (req,res)=>{
     await user.save()
     res.status(201).json("User created Successfully")
    } catch (error) {
-    res.status(500).json(error.message);
+     next(error);   
    }
 
 };
