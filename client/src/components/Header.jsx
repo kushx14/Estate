43483f1bb88a1c2a6b4a939afba5b937be86a518
kushx14 <157268371+kushx14@users.noreply.github.com>
2025-03-21 11:10:50 +0530

    sign up full

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link>
+        <Link to="/">
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
           <span className="text-slate-500">Sahand</span>
           <span className="text-slate-700">Estate</span>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server:{
+    proxy:{
+     "/api":{
+      target:'http://localhost:3000',
+      secure:false,
+     }
+  },
+},
+  plugins: [react()], 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
