d1fbd2c3ef41cac293f92cee4d29ca6a6e932972
kushx14 <157268371+kushx14@users.noreply.github.com>
2025-03-22 02:09:46 +0530

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup=async (req,res,next)=>{
    const {username ,email,password}=req.body;
    const hashedpassword=bcryptjs.hashSync(password,10);
@@ -12,3 +14,25 @@ export const signup=async (req,res,next)=>{
    }
 
 };
+
+export const  signin=async(req,res,next)=>{
+     const {email,password}=req.body;
+     try {
+      const validUser= await User.findOne({email});
+      if (!validUser)
+        {return next(errorHandler(404,'User not Found'));}
+      
+      const validPassword=bcryptjs.compareSync(password,validUser.password);
+      if (!validPassword)
+        {return next(errorHandler(401,'Invalid Password'));}
+      const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+      const {password: pass, ...res}=validUser._doc
+      res
+         .cookie('access_token',token,{httpOnly:true})
+         .status(200)
+         .json(validUser);
+
+     } catch (error) {
+      next(error);
+     }
+}
