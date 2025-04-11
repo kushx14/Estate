0ba91f393e014f76786885391e7cbca1751d0a2e
kushx14 <157268371+kushx14@users.noreply.github.com>
2025-04-11 10:39:00 +0530

    listing api backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
+import listingRouter from './routes/listing.routes.js'
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.listen(3000,()=>{
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
 
 app.use((err,req,res,next)=>{
     const statuscode=err.statuscode || 500;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,9 +33,33 @@ const userSlice = createSlice({
         state.error = action.payload;
         state.loading = false;
       },
+      deleteUserStart: (state) => {
+        state.loading = true;
+      },
+      deleteUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+      },
+      deleteUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      },
+      signOutUserStart: (state) => {
+        state.loading = true;
+      },
+      signOutUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+      },
+      signOutUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      },
     }
     });
 
-export const {signInSuccess,signInStart,signInFailure,updateUserFailure,updateUserSuccess,updateUserStart}=userSlice.actions;
+export const {signInSuccess,signInStart,signInFailure,signOutUserFailure,signOutUserStart,signOutUserSuccess,updateUserFailure,updateUserSuccess,deleteUserFailure,deleteUserStart,deleteUserSuccess,updateUserStart}=userSlice.actions;
 
 export default userSlice.reducer;
