f99f6ec2e83a8878afb3800bd3ba07e5a5eb703c
kushx14 <157268371+kushx14@users.noreply.github.com>
2025-04-11 11:43:16 +0530

    listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRouter from './components/PrivateRouter'
+import CreateListing from './pages/CreateListing'
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
       <Route path='/about' element={<About />}></Route>
       <Route element={<PrivateRouter/>}>
       <Route path='/profile' element={<Profile />}></Route>
+      <Route path='/create-listing' element={<CreateListing />} />
       </Route>
      </Routes>
     </BrowserRouter>
